8e9c06508280c1455117bd53fb67901cb6badf13
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2018-04-15 17:55:17 -0700

    Add a proper src/index file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,3 @@
-import React from 'react';
-
-function compose(...contextConsumers) {
-  return class Composed extends React.PureComponent {
-    render() {
-      const composeHelper = (remainingContextConsumers, collectedContexts = []) => {
-        if (!remainingContextConsumers.length) {
-          return this.props.children(...collectedContexts);
-        }
-
-        const [Next, ...rest] = remainingContextConsumers;
-        return (
-          <Next>
-            {context => composeHelper(rest, [...collectedContexts, context])}
-          </Next>
-        );
-      }
-
-      return composeHelper(contextConsumers);
-    }
-  }
-}
+import compose from './compose';
 
 export default compose;
-
