edfab0a75cc2cd7b65606bd5ab0e6c076440e413
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2018-04-15 17:21:48 -0700

    Set up build system with babel and rollup

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: pkg.main,
+    format: 'cjs',
+  },
+  external: [
+    'react',
+    'react-dom',
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
+}
